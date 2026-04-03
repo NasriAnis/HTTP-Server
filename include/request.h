dc840df9b59f1b797fa95b4d7dc3050c6f741be5
NasriAnis <anisnasri322@gmail.com>
2026-04-03 22:05:42 +0100

    Cleaning

--- include/request.h
+++ include/request.h
@@ -6,19 +6,19 @@
 #define MAX_HEADERS 50
 
 typedef struct {
-    char *key;
-    char *value;
+  char *key;
+  char *value;
 } http_header_t;
 
 typedef struct {
-    char *method;           // GET, POST, etc.
-    char *path;             // /index.html
-    char *version;          // HTTP/1.1
-    http_header_t headers[MAX_HEADERS];
-    int header_count;
-    char *body;
-    size_t body_len;
-    char *raw_data;         // Pointer to the full allocated buffer for easy freeing
+  char *method;  // GET, POST, etc.
+  char *path;    // /index.html
+  char *version; // HTTP/1.1
+  http_header_t headers[MAX_HEADERS];
+  int header_count;
+  char *body;
+  size_t body_len;
+  char *raw_data; // Pointer to the full allocated buffer for easy freeing
 } http_request_t;
 
 // Function prototypes
